7a116db6a0bc100df314b5f6647c7d7ce01ce923
LuxXx <pr0v0ziert@gmx.de>
2017-06-03 14:04:01 +0200

    maven project generator script

---
+++ mvn-api-generate.bat
@@ -0,0 +1,1 @@
+mvn archetype:generate -DarchetypeArtifactId=jersey-quickstart-grizzly2 -DarchetypeGroupId=org.glassfish.jersey.archetypes -DinteractiveMode=false -DgroupId=league -DartifactId=whatsmyrole -Dpackage=whatsmyrole -DarchetypeVersion=2.25
