cb4aa96fd60456c385fe67506e3fcf9310b3088f
LuxXx <pr0v0ziert@gmx.de>
2017-06-09 15:51:50 +0200

    added information download procedure

--- whatsmyrole/src/main/java/whatsmyrole/LaneData.java
+++ whatsmyrole/src/main/java/whatsmyrole/LaneData.java
@@ -1,7 +1,18 @@
 package whatsmyrole;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class LaneData {
-		
+	
+	private static int count = 0;
+	
 	private static String URL = "http://champion.gg/champion/";
 	private static String[] champions = {
 			"Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Ashe","Aurelion Sol","Azir",
@@ -20,19 +31,49 @@ public class LaneData {
 			"Xerath","Xin Zhao","Yasuo","Yorick","Zac","Zed","Ziggs","Zilean","Zyra"
 	};
 	
-	
-	
-	public static String[] doesNotExist(Champion[] champion) {
-		// Fill this with love
-		return null;
-	}
+	private static HashMap<String, ChampionData> map = new HashMap<>();
 
 	public static double getPercentage(Champion champion, Role r) {
-		// TODO: pass the information
-		return Math.random();
+		ChampionData cd = map.get(champion.getName());
+				
+		for (ChampionData.Role role : cd.roles) {
+			String r1 = r.toString();
+			String r2 = role.title.substring(0, 3).toUpperCase();
+			if (r2.equals("JUN"))
+				r2 = "JGL";
+			
+			if (r1.equals(r2))
+				return role.percentPlayed;
+		}
+		return 0;
 	}
 	
 	public static void init() {
-		// Download information and store them
+		System.out.println("Trying to download ChampionData");
+		Gson gson = new GsonBuilder().create();
+		
+		for (String champ : champions) {
+			String json = downloadJSON(champ);
+			ChampionData cd = gson.fromJson(json, ChampionData.class);
+			String key = cd.name.replace("'", "").replace(".", "").toLowerCase();
+			map.put(key, cd);
+			System.out.println(cd);
+		}
+		System.out.println("Downloaded " + count + " Champions of " + champions.length);
+	}
+	public static String downloadJSON(String name) {
+		try {
+			Document d = Jsoup.connect(URL + name).get();
+			count++;
+			for (Element e : d.getAllElements()) {
+				if (e.tagName().equals("script") && e.data().contains("matchupData.champion")) {
+					return e.data().split("\n")[1].substring(25);
+				}
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "{}";
 	}
 }
